7e84a3a9acd26af6f208c1e6efeeb446f3398219
Ernest <musaubrian15@gmail.com>
2023-02-02 12:11:14 +0300

    chore: Update class to accept a list of strings fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 """
-module obfuscates log messages
+module obfuscates log msgs
 """
 
 from typing import List
 import re
+import logging
 
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
