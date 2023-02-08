527ccb0e3bef667e07a548fb132de3ef0b48e206
Ernest <musaubrian15@gmail.com>
2023-02-08 11:06:28 +0300

    chore: implement method to decoded value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,20 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Inherits class Auth"""
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Return None if authorization_header is None
+        Return None if authorization_header is not a string
+        Return None if authorization_header doesn’t start by Basic
+            (with a space at the end)
+        Otherwise, return the value after Basic (after the space)
+        """
+        if (
+            not authorization_header
+            or type(authorization_header) != str
+            or not authorization_header.startswith("Basic")
+        ):
+            return
+        return "".join(authorization_header.split(" ")[1:])
