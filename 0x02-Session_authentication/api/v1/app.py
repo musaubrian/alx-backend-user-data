35e0c12e5a323211e7e73dd43807c52083cb1f46
Ernest <musaubrian15@gmail.com>
2023-02-10 13:11:04 +0300

    chore: update exluded paths lists
    add /api/v1/auth_session/login/ to the exluded_list

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,7 +54,11 @@ def before_request():
     """
     Filter requests
     """
-    exclude_list = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    exclude_list = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/" "/api/v1/auth_session/login/",
+    ]
 
     if auth is not None:
         if auth.require_auth(
@@ -62,7 +66,8 @@ def before_request():
             exclude_list,
         ):
             if auth.authorization_header(request) is None:
-                abort(401)
+                if auth.session_cookie(request) is None:
+                    abort(401)
 
             if auth.current_user(request) is None:
                 abort(403)
