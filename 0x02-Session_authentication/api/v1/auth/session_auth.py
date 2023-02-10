7c3b0d4d386240345d2dd4c9e65f4183ec7cb986
Ernest <musaubrian15@gmail.com>
2023-02-10 12:55:12 +0300

    chore(auth): return userID using the key sessionID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,12 @@ class SessionAuth(Auth):
 
         session_id = str(uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns userID based on a sessionID"""
+        if not session_id or type(session_id) != str:
+            return None
+
+        return SessionAuth.user_id_by_session_id.get(session_id, None)
