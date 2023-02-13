13a16f4d074cb5ce9a83d75cd7ae6a974d691f8f
Ernest <musaubrian15@gmail.com>
2023-02-13 15:29:10 +0300

    chore: implement register_user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """Module deals with authentication"""
-from db import DB
+from db import DB, NoResultFound
 from bcrypt import gensalt, hashpw
+
+
+def _hash_password(password: str) -> bytes:
+    """Returns bytes as a salted hash of the input password"""
+    hashed_pwd = hashpw(password.encode("utf-8"), gensalt())
+
+    return hashed_pwd
 
 
 class Auth:
@@ -10,8 +17,12 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
-        """Returns bytes as a salted hash of the input password"""
-        hashed_pwd = hashpw(password.encode("utf-8"), gensalt())
-
-        return hashed_pwd
+    def register_user(self, email: str, password: str):
+        """ """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            new_user = self._db.add_user(email, str(hashed_pwd))
+            return new_user
